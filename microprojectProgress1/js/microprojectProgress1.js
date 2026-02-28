965485ac79d968eeb4496fe2c72d415d58c1deaf
noellecurry <924735211@sfsu.edu>
2026-02-27 19:11:09 -0800

    Css upload

--- microprojectProgress1/js/microprojectProgress1.js
+++ microprojectProgress1/js/microprojectProgress1.js
@@ -1,3 +1,28 @@
-function showCircle() {
-  document.getElementById("circle").style.display = "block";
+let button;
+let canvasCreated = false;
+
+function setup() {
+  
+  // Create a button and set its value to 0.
+  // Place the button beneath the canvas.
+  button = createButton('breathe deep');
+  button.mousePressed(showCanvas);
 }
+
+function draw() {
+  if (!canvasCreated) {
+  background(200);
+  circle(250, 250, 200);
+  }
+
+}
+
+
+  function showCanvas() {
+    if (!canvasCreated) {
+     createCanvas(500, 500);
+     canvasCreated = true;
+    }
+  
+}
+
